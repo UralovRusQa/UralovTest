a65a6efe13884aee8686c7fc0c8e445f702f9a33
UralovRusQa <rus.uralov@yandex.ru>
2024-03-25 20:25:39 +0500

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    Test test = new Test();
+    test.test1();
+    test.test2();
+    test.test3();
+  }
+}
